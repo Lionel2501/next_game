dfc8de0f4432e719b2c772384e28bc1ef9e28cb4
Lionel2501 <lionelcassar92@gmail.com>
2024-04-08 01:31:55 -0300

    new data structure response

--- simulacion_temporada_2.py
+++ simulacion_temporada_2.py
@@ -220,7 +220,7 @@ def getResultado(equipoLocal, equipoVisitante, contexto_global, fecha, score_rea
         'equipo': equipoVisitante
     }
     
-    count_total = 0
+    count_games = 0
     
     for row in contexto_global:
         resultado = row['resultado']
@@ -237,19 +237,78 @@ def getResultado(equipoLocal, equipoVisitante, contexto_global, fecha, score_rea
         if resultadoLocal == resultadoVisitante:
             countEmpate['count'] += 1
         
-        count_total = count_total + 1
+        count_games = count_games + 1
             
     mayor_count = max(countEmpate['count'], countLocal['count'], countVisitante['count'])
-    pronostico = 'empate'
+    """     pronostico = 'empate'
+    pierde_prono = 'empate'
     if mayor_count == countLocal['count']:
         pronostico = countLocal['equipo']
+        pierde_prono = equipoVisitante
     if mayor_count == countVisitante['count']:
         pronostico = countVisitante['equipo']
+        pierde_prono = equipoLocal """
         
+    pronostico, pierde_prono = determinar_pronosticos(countLocal, countVisitante, equipoLocal, equipoVisitante, mayor_count)
     total = len(contexto_global)
     porcentaje_calcul = mayor_count / total * 100
     porcentaje = round(porcentaje_calcul, 2)
     
+    """     score_partes = score_real.split(' - ')
+    score_local = score_partes[0]
+    score_visitante = score_partes[1]
+    score_local = int(score_local)
+    score_visitante = int(score_visitante) """
+    
+    resultado_real = setResultadoReal(score_real)
+    """     if score_local > score_visitante:
+        resultado_real  = equipoLocal
+    if score_visitante > score_local:
+        resultado_real = equipoVisitante """
+    
+    valido = 0
+    if resultado_real == pronostico:
+        valido = 1 
+        
+    """     pierde_valido = 0
+    pierde_porcentaje = 0
+    if resultado_real != pierde_prono and pierde_prono != 'empate':
+        pierde_valido = 1 
+        pierde_porcentaje_calcul = (mayor_count + countEmpate['count']) / total * 100
+        pierde_porcentaje = round(pierde_porcentaje_calcul, 2) """
+        
+    pierde_valido, pierde_porcentaje = calcular_pierde_valido_y_porcentaje(resultado_real, pierde_prono, mayor_count, countEmpate, total)
+    
+    response = {
+        'year': '2021-2022',
+        'fecha': fecha,
+        'local': equipoLocal,
+        'visitante': equipoVisitante,
+        'resultado_real': resultado_real,
+        'count_games': count_games,
+        'pronostico': pronostico,
+        'porcentaje': porcentaje,
+        'valido': valido,
+        'pierde_prono': pierde_prono,
+        'pierde_porcentaje': pierde_porcentaje,
+        'pierde_valido': pierde_valido,
+    }
+    
+    return response
+
+def calcular_pierde_valido_y_porcentaje(resultado_real, pierde_prono, mayor_count, countEmpate, total):
+    pierde_valido = 0
+    pierde_porcentaje = 0
+
+    if resultado_real != pierde_prono:
+        pierde_valido = 1 
+        
+    pierde_porcentaje_calcul = (mayor_count + countEmpate['count']) / total * 100
+    pierde_porcentaje = round(pierde_porcentaje_calcul, 2)
+
+    return pierde_valido, pierde_porcentaje
+    
+def setResultadoReal(score_real):
     score_partes = score_real.split(' - ')
     score_local = score_partes[0]
     score_visitante = score_partes[1]
@@ -261,55 +320,50 @@ def getResultado(equipoLocal, equipoVisitante, contexto_global, fecha, score_rea
         resultado_real  = equipoLocal
     if score_visitante > score_local:
         resultado_real = equipoVisitante
+        
+    return resultado_real
+
+def determinar_pronosticos(countLocal, countVisitante, equipoLocal, equipoVisitante, mayor_count):
+    pronostico = 'empate'
+    pierde_prono = 'empate'
     
-    valido = 0
-    if resultado_real == pronostico:
-        valido = 1 
-    
-    response = {
-        'local': equipoLocal,
-        'visitante': equipoVisitante,
-        'resultado_real': resultado_real,
-        'pronostico': pronostico,
-        'valido': valido,
-        'count_local': countLocal['count'],
-        'count_empate': countEmpate['count'],
-        'count_visitante': countVisitante['count'],
-        'count_total': count_total,
-        'porcentaje': porcentaje,
-        'fecha': fecha,
-        'year': '2022-2023'
-    }
-    
-    return response
+    if mayor_count == countLocal['count']:
+        pronostico = countLocal['equipo']
+        pierde_prono = equipoVisitante
+    if mayor_count == countVisitante['count']:
+        pronostico = countVisitante['equipo']
+        pierde_prono = equipoLocal
+
+    return pronostico, pierde_prono
     
 def postResultadoSimulacion(result_data):   
     cursor.execute('''INSERT INTO prono_2022 (
+        year,
+        fecha, 
         local, 
         visitante, 
         resultado_real, 
+        count_games,
+        porcentaje, 
         pronostico, 
         valido, 
-        count_local,
-        count_empate,
-        count_visitante,
-        count_total,
-        porcentaje, 
-        fecha, 
-        year) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)''', 
-        (result_data['local'], 
+        pierde_prono,
+        pierde_porcentaje,
+        pierde_valido
+    ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)''', (
+        result_data['year'],
+        result_data['fecha'],
+        result_data['local'], 
         result_data['visitante'], 
         result_data['resultado_real'], 
+        result_data['count_games'], 
+        result_data['porcentaje'], 
         result_data['pronostico'], 
         result_data['valido'], 
-        result_data['count_local'], 
-        result_data['count_empate'], 
-        result_data['count_visitante'], 
-        result_data['count_total'], 
-        result_data['porcentaje'], 
-        result_data['fecha'], 
-        result_data['year']))
-    
+        result_data['pierde_prono'], 
+        result_data['pierde_porcentaje'], 
+        result_data['pierde_valido']
+    ))
     
     conexion.commit()
     
